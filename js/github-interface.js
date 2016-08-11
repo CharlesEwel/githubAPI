7e78de4944f708598060380fae2d0fe88e3d5cbd
Charles Ewel <Charles.Ewel@gmail.com>
2016-08-11 08:10:24 -0700

    fixed issue with list not clearing on new search

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,6 +2,7 @@ var gitHubRequest = require('./../js/github.js').gitHubModule;
 
 $(document).ready(function(){
   $("#user-form").submit(function(event){
+    $("#repository-list").empty()
     event.preventDefault();
     var username = $("#user-name").val();
     var newGitHubRequest = new gitHubRequest(username);
